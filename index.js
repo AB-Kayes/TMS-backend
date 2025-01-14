5ef17d64ba1215432b91d890e5ea35023ddaf758
Kayes <arifbenkayes@gmail.com>
2025-01-14 18:54:19 +0600

    completed the authentication

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./src/routes/userRoutes');
+
+dotenv.config(); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log(err));
 
 app.use('/api/users', userRoutes);
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,7 @@ const {registerUser, loginUser} = require('../controllers/userControllers');
 
 const router = express.Router();
 
-router.post('./register', registerUser);
-router.post('./login', loginUser);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
